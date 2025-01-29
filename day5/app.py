74ec7f7ec13695e32c43561793ec498b6795370b
Balaji2804 <137748735+Balaji2804@users.noreply.github.com>
2025-01-29 17:23:03 +0530

    Update app.py

--- day5/app.py
+++ day5/app.py
@@ -1,71 +1,82 @@
-import sys
-import os
-
-# Add the parent directory to sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
-from fastapi import FastAPI,Request, Depends, HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from dotenv import load_dotenv
-from itsdangerous import BadSignature
-from mangum import Mangum
-from fastapi.middleware.cors import CORSMiddleware
-
-# Load environment variables from the .env file
-load_dotenv()
-
-# Import routes only after loading environment variables
-from app.auth.routes import auth_router, get_current_user, serializer
-from app.failover.routes import failover_router  #
-from app.onboard.routes import onboard_router
-
-app = FastAPI()
-
-origins = [
-    'http://localhost:3000',
-    'http://localhost:8080',
-    'http://localhost:8000',
-    "http://127.0.0.1:8080",
-    ]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
-
-# Include Routers
-app.include_router(auth_router, prefix="/auth")
-app.include_router(failover_router, prefix="/failover")
-app.include_router(onboard_router, prefix="/onboard")
-
-# # Mount static files
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-#
-# # Set up Jinja2 templates
-# templates = Jinja2Templates(directory="templates")
-# # Mount static files from the React build directory
-app.mount("/", StaticFiles(directory="../my-resiliencyapp/build", html=True), name="react")
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    user_info_cookie = request.cookies.get("user_info")
-    if user_info_cookie:
-        try:
-            user_info = serializer.loads(user_info_cookie)
-            return HTMLResponse(open("../my-resiliencyapp/build/index.html").read())
-            # return templates.TemplateResponse("dashboard.html", {"request": request, "user_info": user_info})
-        except BadSignature:
-            return RedirectResponse("/signin")
-    # return templates.TemplateResponse("login.html", {"request": request})
-    return HTMLResponse(open("../my-resiliencyapp/build/index.html").read())
-
-# AWS Lambda handler for deployment using
-
-# AWS Lambda handler for deployment using Mangum
-handler = Mangum(app)
+[
+  {
+    "name": "${container_name}",
+    "image": "${ecr_url}:${build_version}",
+    "essential": true,
+    "environment": [
+      {
+        "name": "build_version",
+        "value": "${build_version}"
+      },
+      {
+        "name": "stage",
+        "value": "${stage}"
+      },
+      {
+        "name": "region",
+        "value": "${region}"
+      },
+      {
+        "name": "kinesis_stream",
+        "value": "${kinesis_stream}"
+      },
+      {
+        "name": "kinesis_region",
+        "value": "${kinesis_region}"
+      }
+    ],
+    "entryPoint": [
+      "sh",
+      "-c"
+    ],
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80,
+        "protocol": "tcp"
+      },
+      {
+        "containerPort": 5170,
+        "hostPort": 5170,
+        "protocol": "tcp"
+      }
+    ],
+    "command": [
+      "./start.sh | tee >(nc localhost 5170)"
+    ],
+    "logConfiguration": {
+      "logDriver": "awsfirelens",
+      "options": {
+        "Name": "kinesis",
+        "region": "${kinesis_region}",
+        "stream": "${kinesis_stream}",
+        "log_key": "log",
+        "auto_create_stream": "true"
+      }
+    }
+  },
+  {
+    "name": "log-router",
+    "image": "amazon/aws-for-fluent-bit:latest",
+    "essential": true,
+    "firelensConfiguration": {
+      "type": "fluentbit"
+    },
+    "portMappings": [
+      {
+        "containerPort": 5170,
+        "hostPort": 5170,
+        "protocol": "tcp"
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "${log_group_name}",
+        "awslogs-region": "${region}",
+        "awslogs-create-group": "true",
+        "awslogs-stream-prefix": "/ecs/${stack_name}"
+      }
+    }
+  }
+]
